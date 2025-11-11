a08111df98fce900690a2ca571133d509f61f7a6
Farizal Elsyahfi <farizaelsyahfi@gmail.com>
2025-11-11 18:42:17 +0700

    ubah warna

--- add.php
+++ add.php
@@ -16,6 +16,7 @@ if (!isset($_SESSION['login_user'])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
 </head>
+<!-- perubahan warna -->
 <body class="bg-dark text-light">
 <div class="container mt-5">
   <h3 class="mb-4 text-success">Tambah Data Kalibrasi</h3>
